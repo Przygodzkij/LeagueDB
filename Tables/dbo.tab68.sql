4a5a37926f41349891459f82c6ea64984f305ccc
Jakub Przygodzki <przygodzki@tkomp.pl>
2023-10-11 10:09:36 +0200

    Init

---
+++ Tables/dbo.tab68.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[tab68]
+(
+[col0] [int] NULL,
+[col1] [int] NULL,
+[col2] [int] NULL,
+[col3] [int] NULL,
+[col4] [int] NULL,
+[col5] [int] NULL,
+[col6] [int] NULL,
+[col7] [int] NULL,
+[col8] [int] NULL,
+[col9] [int] NULL
+) ON [PRIMARY]
+GO
